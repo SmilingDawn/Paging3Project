5437343af4cc39bf1052531b0b430811cc7d6573
jongmin.lee <lee30345@naver.com>
2022-08-11 17:03:24 +0900

    - EditText를 이용한 검색 기능 추가

--- app/src/main/java/com/example/paging3project/MainActivity.kt
+++ app/src/main/java/com/example/paging3project/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.paging3project
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.paging3project.adapter.GitRepoListAdapter
@@ -12,6 +17,7 @@ import com.example.paging3project.databinding.ActivityMainBinding
 import com.example.paging3project.repository.GitRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
         binding.activity = this
+        binding.viewModel = viewModel
 
         binding.rvView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
@@ -49,19 +56,31 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        lifecycleScope.launch {
+            adapter.loadStateFlow.collectLatest { loadStates ->
+                binding.progressCircular.isVisible = loadStates.refresh is LoadState.Loading
+
+                val error = when {
+                    loadStates.prepend is LoadState.Error -> loadStates.prepend as LoadState.Error
+                    loadStates.append is LoadState.Error -> loadStates.append as LoadState.Error
+                    loadStates.refresh is LoadState.Error -> loadStates.refresh as LoadState.Error
+                    else -> null
+                }
+                error?.let {
+                    Toast.makeText(this@MainActivity, it.error.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     /**
      * Binding Method
      */
-    fun onClickSearch1() {
-        viewModel.setSearchGitRepo("rxSwift")
-    }
-
-    /**
-     * Binding Method
-     */
-    fun onClickSearch2() {
-        viewModel.setSearchGitRepo("Kotlin")
+    fun onClickSearch() {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.etSearchInput.windowToken, 0)
+        viewModel.searchKeyword()
     }
 }
--- app/src/main/java/com/example/paging3project/MainViewModel.kt
+++ app/src/main/java/com/example/paging3project/MainViewModel.kt
@@ -5,22 +5,42 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.paging3project.model.RepositoryModel
 import com.example.paging3project.repository.GitRepository
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 
 class MainViewModel(private val repository: GitRepository): ViewModel() {
-    private val _searchKeyword: MutableLiveData<String> = MutableLiveData("")
-    private val searchKeyword: LiveData<String> get() = _searchKeyword
+    private val _searchKeyword: MutableLiveData<String> = MutableLiveData("android") // Git Search api는 "q=" 파라미터 값이 공백일경우 422 ErrorCode를 반환
 
-    fun setSearchGitRepo(keyword: String) {
-        _searchKeyword.value = keyword
-    }
+    private val _inputKeyword: MutableLiveData<String> = MutableLiveData("")
+    val inputKeyword: LiveData<String> get() = _inputKeyword
 
-    var searchGitRep = searchKeyword.switchMap {
+    private var searchKeywordJob: Job? = null
+
+    var searchGitRep = _searchKeyword.switchMap {
         repository.getGitRepository(it).cachedIn(viewModelScope).asLiveData()
     }
 
     fun defaultSearch(): Flow<PagingData<RepositoryModel>> {
         return repository.getGitRepository("JavaScript").cachedIn(viewModelScope)
+    }
+
+    fun searchKeyword() {
+        searchKeywordJob?.cancel()
+        _searchKeyword.value = _inputKeyword.value
+    }
+
+    /**
+     * Binding Method
+     */
+    fun onSearchKeywordTextChanged(sequence: CharSequence, start: Int, before: Int, count: Int) {
+        val keyword = sequence.toString()
+        _inputKeyword.value = keyword
+
+        searchKeywordJob?.cancel()
+        searchKeywordJob = CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            _searchKeyword.postValue(keyword)
+        }
     }
 
     class Factory(private val repository: GitRepository): ViewModelProvider.Factory {
